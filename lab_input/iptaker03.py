b756ba6fdaafba8314232ded0214095605513644
Maya Marks <maya.marks.dc@gmail.com>
2020-08-10 17:08:17 -0400

    message print from a list

---
+++ lab_input/iptaker03.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+user_input = input("what's your name:")
+
+
+print("Hello ", user_input)
+print( "Let me get lunch early!")
---
+++ lab_input/iptaker04.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+name = input("What's your name:")
+day = input("What day of the week it is:")
+print(f"Hello, {name}! Happy {day}!")
+
