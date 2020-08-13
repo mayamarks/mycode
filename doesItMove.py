4eb312408f5912820bf570497b9ea026073b0baa
Maya Marks <maya.marks.dc@gmail.com>
2020-08-13 11:22:34 -0400

    fixing git issues from VisualStudio Code

--- doesItMove.py
+++ doesItMove.py
@@ -19,39 +19,38 @@ def escape():
 # first question
 while i == 0:
     question1 = input("\nDoest it move?")
-    if question1.capitalize() == "Q":
+    if question1.strip().capitalize() == "Q":
         escape()
         break
-    elif question1.capitalize() == "Y" or question1.capitalize() == "YES":
+    elif question1.strip().capitalize() == "Y" or question1.strip().capitalize() == "YES":
         i += 1
         # second question
         while i == 1:
             question2 = input("\nShould it?")
-            if question1.capitalize() == "Q":
-                escape()
+            if question2.strip().capitalize() == "Q":
                 break
-            elif question2.capitalize() == "Y" or question2.capitalize() == "YES":
+            elif question2.strip().capitalize() == "Y" or question2.strip().capitalize() == "YES":
                 print("\nNo problem1")
                 i += 1
-            elif question2.capitalize() == "N" or question2.capitalize() == "NO":
+            elif question2.strip().capitalize() == "N" or question2.strip().capitalize() == "NO":
                 print("\nUse duck tape")
                 i += 1
             # 'i' does not increase or decrease, so the question will remain the same
             else:
                 err()
 
-    elif question1.capitalize() == "N" or question1.capitalize() == "NO":
+    elif question1.strip().capitalize() == "N" or question1.strip().capitalize() == "NO":
         i += 1
         # second question
         while i == 1:
             question2 = input("\nShould it?")
-            if question1.capitalize() == "Q":
+            if question2.strip().capitalize() == "Q":
                 escape()
                 break
-            elif question2.capitalize() == "Y" or question2.capitalize() == "YES":
+            elif question2.strip().capitalize() == "Y" or question2.strip().capitalize() == "YES":
                 print("\nGlue it!")
                 i += 1
-            elif question2.capitalize() == "N" or question2.capitalize() == "NO":
+            elif question2.strip().capitalize() == "N" or question2.strip().capitalize() == "NO":
                 print("\nNo problem")
                 i += 1
             # i does not increase or decrease, so the question will remain the same
@@ -59,3 +58,4 @@ while i == 0:
                 err()
     else:
         err()
+
